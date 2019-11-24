f6ecf9d65094b5a4de121222c99f02995a89eb17
Ashish Patel <ashishpatel.1992@gmail.com>
2019-11-24 23:33:10 +0530

    Added comments and taking IP from command line

--- rpcserv_client.cc
+++ rpcserv_client.cc
@@ -1,11 +1,13 @@
-
+/**
+ *  Description: RPC Client - sends function name and arguments to server
+ *  Developed by: Ashish Patel & Nand Parikh
+ **/
+ 
 #include <iostream>
-#include <memory>
 #include <string>
+#include <arpa/inet.h>
 
 #include <grpcpp/grpcpp.h>
-
-
 #include "rpcserv.grpc.pb.h"
 
 using grpc::Channel;
@@ -16,87 +18,105 @@ using rpcserv::Return;
 using rpcserv::AddRPCService;
 
 #define MAX_MSG_SIZE 1024
+struct rpc_response{
+  bool success;
+  int32_t value;
+};
 
 class AddRPCServiceClient {
- public:
-  AddRPCServiceClient(std::shared_ptr<Channel> channel): stub_(AddRPCService::NewStub(channel)) {}
+  private:
+    std::unique_ptr<AddRPCService::Stub> stub_;
+  public:
+    AddRPCServiceClient(std::shared_ptr<Channel> channel): stub_(AddRPCService::NewStub(channel)) {}
 
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string AddRPC(const std::string& func_name, const std::string& arg_data) {
-    // Data we are sending to the server.
-    Call request;
-    // int args_x[] = {5,10};
-    request.set_name(func_name);
-    // std::cout<<google::protobuf::serializ(args_x);
-    request.set_args(arg_data);
-    // std::string test[255];
-    // request.SerializeToString(test);
-    // std::cout<<test<<"\n";
-    // request.ParseFromString(*test);
-    // std::cout<<test<<"\n";
+    // Assembles the client's payload, sends it and presents the response back
+    // from the server.
+    rpc_response AddRPC(const std::string& func_name, const std::string& arg_data) {
+      // Data we are sending to the server.
+      Call request;
+      request.set_name(func_name);
+      request.set_args(arg_data);
+      
+      std::string serializedData;
+      request.SerializeToString(&serializedData);
+      
+      // Container for the data we expect from the server.
+      Return reply;
+      
+      // Context for the client. It could be used to convey extra information to
+      // the server and/or tweak certain RPC behaviors.
+      ClientContext context;
     
-    // Container for the data we expect from the server.
-    Return reply;
-    // reply.SerializeToString()
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-  
-    // The actual RPC.
-    Status status = stub_->AddRPC(&context, request, &reply);
+      // RPC Call
+      Status status = stub_->AddRPC(&context, request, &reply);
 
-    // Act upon its status.
-    if (status.ok()) {
-      return (reply.message()+" "+reply.value());
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
+      // Action for RCP response
+      if (status.ok()) {
+        rpc_response rpc_r;
+        rpc_r.success = reply.success();
+        rpc_r.value = stoi(reply.value());
+        return rpc_r;
+        // return (reply.message()+" "+reply.value());
+      } else {
+        rpc_response rpc_r;
+        rpc_r.value = -1;
+        rpc_r.success = false;
+        std::cout << status.error_code() << ": " << status.error_message()
+                  << std::endl;
+        return rpc_r;
+      }
     }
 
-    // Call hr = Call_INIT;
-  }
 
- private:
-  std::unique_ptr<AddRPCService::Stub> stub_;
 };
 void help(){
   std::cout<<"\n Only Two actions permitted: \n\t1) add \n\t2) gettotal";
-  std::cout<<"\n use ./rpcserv_client add x y\n\t where x and y can be any integer number";
+  std::cout<<"\n use ./rpcserv_client [ip] [port] add x y\n\t where x and y can be any integer number";
   std::cout<<"\n use ./rpcserv_client gettotal\n\t Returns the total sum computer in Server\n";
 }
 
 
 int main(int argc, char* argv[]) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  // intro();
-  AddRPCServiceClient AddRPCService(grpc::CreateChannel("localhost:9999", grpc::InsecureChannelCredentials()));
-  if(argc<2){
+  struct in_addr addr;
+  
+  if(argc<4){
+    std::cout<<"\n===================";
     std::cout<<"\nInvalid RPC Call ";
+    std::cout<<"\n===================";
     help();
     return 0;
   }
+  char* ip_addr = argv[1];
+  if(!inet_aton(ip_addr,&addr)){
+    std::cout<<"\n Invalid IP address";
+    return 0;
+  }
+  
+  std::string port_no = argv[2];
+  std::string socket_addr = argv[1];
+  socket_addr+=":";
+  socket_addr+=argv[2];
+  
+  AddRPCServiceClient AddRPCService(grpc::CreateChannel(socket_addr, grpc::InsecureChannelCredentials())); 
   std::string func_name;
-  func_name = argv[1];
+  
+  func_name = argv[3];
   std::string arg_data;
   
-  for (int i = 2; i < argc; ++i){
-        if(func_name == "add"){
-            arg_data = arg_data+argv[i]+" ";
-            // cout << argv[i] << "\n"; 
-        }else break;
-    } 
+  for (int i = 4; i < argc; ++i){
+    arg_data = arg_data+argv[i]+" ";
+  } 
   
-  std::string reply = AddRPCService.AddRPC(func_name,arg_data);
-  if(reply != "")
-    if(func_name == "add")
-      std::cout << "Sum received: " << reply <<"\n";
-    else if(func_name == "gettotal")
-      std::cout<<" Total is "<< reply<<"\n";
-    else  std::cout<<" ";
+  rpc_response reply = AddRPCService.AddRPC(func_name,arg_data);
+
+  if(reply.success){
+    std::cout<<"\nAction: "<<func_name;
+    std::cout<<"\nResult: "<<reply.value;
+    std::cout<<"\n";
+  }else{
+    std::cout<<"\nAction: "<<func_name<<" does not exist\n";
+    help();
+  }
+
   return 0;
 }
--- rpcserv_server.cc
+++ rpcserv_server.cc
@@ -1,32 +1,14 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- *
- */
-// protoc -I . --grpc_out=. --plugin=protoc-gen-grpc=grpc_cpp_plugin rpcserv.proto
-// protoc -I . --cpp_out=. ../../protos/rpcserv.proto
+/**
+ *  Description: RPC Client - sends function name and arguments to server
+ *  Developed by: Ashish Patel & Nand Parikh
+ **/
 
 #include <iostream>
-#include <memory>
 #include <string>
-#include<bits/stdc++.h>
+#include <sstream>
+#include <arpa/inet.h>
 
 #include <grpcpp/grpcpp.h>
-
-
 #include "rpcserv.grpc.pb.h"
 
 using grpc::Server;
@@ -36,76 +18,100 @@ using grpc::Status;
 using rpcserv::Call;
 using rpcserv::Return;
 using rpcserv::AddRPCService;
-static uint32_t total =0;
+
+static uint32_t total = 0;
+
 uint32_t add(uint32_t a, uint32_t b){
   total+= (a + b);
   return (a+b);
 }
+
 uint32_t gettotal(void){
   return (total);
 }
+
 void intro(){
-  std::cout<<"\n **************************************************\n";
-  std::cout<<"\n *\tDeveloped by: \n";
-  std::cout<<"\n *\tAshish Patel (2019H1030028G) \n";
-  std::cout<<"\n *\tNand Parikh  (2019H1030022G) \n";
+  std::cout<<"\n **************************************************";
+  std::cout<<"\n *\tDeveloped by: ";
+  std::cout<<"\n *\tAshish Patel (2019H1030028G) ";
+  std::cout<<"\n *\tNand Parikh  (2019H1030022G) ";
   std::cout<<"\n ***************************************************\n";
 }
 using namespace std;
 // Logic and data behind the server's behavior.
 class AddRPCServiceServiceImpl final : public AddRPCService::Service {
   Status AddRPC(ServerContext* context, const Call* request, Return* reply) override {
-    // std::string prefix("Hello ");
-
-    if(request->name() == "add"){
     
-      std::string s = request->args();
+    if(request->name() == "add" || request->name() == "ADD" ){
+      
       uint32_t x,y;
+      std::string s = request->args();
       istringstream istringname(s);
+      
       istringname>> x>>y;
+      
       uint32_t result = add(x,y);
-    
-      reply->set_message("success");
+      
+      reply->set_success(true);
       reply->set_value(std::to_string(result));
     
-    }else if(request->name() == "gettotal"){
+    }else if(request->name() == "gettotal" || request->name() == "GETTOTAL"){
 
-      reply->set_message("success");
+      reply->set_success(true);
       reply->set_value(std::to_string(total));
     
     }
     else{
-      reply->set_message("Request for "+ request->name()+ " not found");
+    
+      reply->set_success(false);
       reply->set_value("-1");
-      // return Status::
+    
     }
     
     return Status::OK;
   }
 };
 
-void RunServer() {
+void RunServer(std::string socket_addr) {
   intro();
-  std::string server_address("0.0.0.0:9999");
+  std::string server_address(socket_addr);
   AddRPCServiceServiceImpl service;
 
   ServerBuilder builder;
   // Listen on the given address without any authentication mechanism.
   builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  // Register "service" as the instance through which we'll communicate with
-  // clients. In this case it corresponds to an *synchronous* service.
+  // Register the Service
   builder.RegisterService(&service);
-  // Finally assemble the server.
+  // Assemble the server.
   std::unique_ptr<Server> server(builder.BuildAndStart());
   std::cout << "Server listening on " << server_address << std::endl;
 
-  // Wait for the server to shutdown. Note that some other thread must be
-  // responsible for shutting down the server for this call to ever return.
+  // Wait for the server to shutdown. 
   server->Wait();
+  
 }
-
+void help(){
+  std::cout<<"\n*********************************";
+  std::cout<<"\n\t Use ./rpcserv_server [port]";
+  std::cout<<"\n*********************************\n";
+  
+}
 int main(int argc, char** argv) {
-  RunServer();
+  struct in_addr addr;
+
+  if(argc<2){
+    std::cout<<"\n===================";
+    std::cout<<"\nInvalid Server Call ";
+    std::cout<<"\n===================";
+    help();
+    return 0;
+  }
+  std::string socket_addr = "0.0.0.0:"; // Listen at all IP's
+  std::string port_no = argv[1];
+  
+  socket_addr+=port_no;
+  
+  RunServer(socket_addr);
 
   return 0;
 }
